89d16fcadfb48484e699853f8e2a90e474235061
conifermjlee <conifermjlee@gmail.com>
2026-09-10 11:13:18 +0900

    fix: 모던 자바 인 액션 - ch2 초기 구조 구현

--- src/main/java/com/apitest/mytest/modern_java/ch02/FilteringApples.java
+++ src/main/java/com/apitest/mytest/modern_java/ch02/FilteringApples.java
@@ -1,7 +1,35 @@
 package com.apitest.mytest.modern_java.ch02;
 
+import com.apitest.mytest.modern_java.ch02.domain.Apple;
+import com.apitest.mytest.modern_java.ch02.domain.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FilteringApples {
     public static void main(String[] args) {
+
         System.out.println("FilteringApples start");
+        Apple apple_1 = new Apple(Color.RED, 1);
+        Apple apple_2 = new Apple(Color.GREEN, 2);
+        Apple apple_3 = new Apple(Color.GREEN, 3);
+
+        List<Apple> inventory = new ArrayList<>();
+        inventory.add(apple_1);
+        inventory.add(apple_2);
+        inventory.add(apple_3);
+
+        List<Apple> greenApples =  filterGreenApples(inventory);
+        System.out.println("녹색 사과 필터링 결과: " + greenApples);
+    }
+
+    public static List<Apple> filterGreenApples (List<Apple> inventory) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple :  inventory) {
+            if (apple.getColor().equals(Color.GREEN)) {
+                result.add(apple);
+            }
+        }
+        return result;
     }
 }
