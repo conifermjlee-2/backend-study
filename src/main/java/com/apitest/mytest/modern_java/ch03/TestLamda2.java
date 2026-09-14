dcc2bc02d8b5d65a16e4a71b52cf01b851cc7e20
conifermjlee <conifermjlee@gmail.com>
2026-09-14 17:02:42 +0900

    fix: TestLamda 수정 2

--- src/main/java/com/apitest/mytest/modern_java/ch03/TestLamda2.java
+++ src/main/java/com/apitest/mytest/modern_java/ch03/TestLamda2.java
@@ -1,21 +1,41 @@
 package com.apitest.mytest.modern_java.ch03;
 
+import com.apitest.mytest.modern_java.ch02.domain.Color;
+import com.apitest.mytest.modern_java.ch03.domain.Apple;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class TestLamda2 {
+
     @FunctionalInterface
-    public interface filterCheck<T>{
+    public interface FilterCheck<T>{
         boolean test(T t);
     }
 
     public static void main(String[] args) {
         System.out.println("start");
+        List<Apple> appleList = new ArrayList<>();
+        appleList.add(new Apple(Color.RED, 1));
+        appleList.add(new Apple(Color.GREEN, 2));
+        appleList.add(new Apple(Color.RED, 3));
 
+        List<Apple> filterMethodList =  filterMethod(appleList, new FilterCheck<Apple>() {
+            @Override
+            public boolean test(Apple apple) {
+                return apple.getColor().equals(Color.RED);
+            }
+        });
+        System.out.println("filterMethodList: " + filterMethodList);
     }
-
-    public static <T> List<T> filterMethod(){
+    public static <T> List<T> filterMethod(List<T> list, FilterCheck t){
         List<T> resultList = new ArrayList<>();
+        for (T fruit : list) {
+            if (t.test(fruit)) {
+                resultList.add(fruit);
+            }
+
+        }
         return resultList;
     }
 }
